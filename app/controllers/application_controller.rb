17318093d17874143f57094155703cc1e552a93d
Kelin Field <kfield111@hotmail.com>
2020-10-31 03:26:14 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,7 +29,7 @@ class ApplicationController < Sinatra::Base
 
 
   get '/articles/:id'  do
-    @article = Article.find_by(id: params[:id]) 
+    @article = Article.find_by(id: params[:id])
     erb :show
   end
 
