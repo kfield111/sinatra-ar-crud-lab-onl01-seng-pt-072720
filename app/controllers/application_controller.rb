5b56e7b8865bd31283ae4b9ac33ef4258a1b815b
Kelin Field <kfield111@hotmail.com>
2020-10-31 03:35:30 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,7 +34,7 @@ class ApplicationController < Sinatra::Base
   end
 
 
-  get 'articles/:id/edit' do
+  get '/articles/:id/edit' do
     @article = Article.find_by(id: params[:id])
     erb :edit
   end
