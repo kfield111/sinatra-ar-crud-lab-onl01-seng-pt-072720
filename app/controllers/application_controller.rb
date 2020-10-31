80fa76f07a8008053beafa95bc15aff4cbf09c7f
Kelin Field <kfield111@hotmail.com>
2020-10-31 02:56:31 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,7 +24,7 @@ class ApplicationController < Sinatra::Base
   post '/articles' do
     Article.create(title: params[:title])
 
-    redirect  :articles/:id
+    redirect to '/articles/:id'
   end
 
   get '/articles/:id'  do
