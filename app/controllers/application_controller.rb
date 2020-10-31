16ede03c8117929fac264cd3676194c433659192
Kelin Field <kfield111@hotmail.com>
2020-10-31 03:09:37 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,18 +15,20 @@ class ApplicationController < Sinatra::Base
     erb :new
   end
 
-  get '/articles' do
-    @articles = Articles.all
-
-    erb :index
-  end
-
   post '/articles' do
     data = params
     @article = Article.create(params)
 
     redirect to "/articles/#{@article.id}"
   end
+
+  get '/articles' do
+    @articles = Articles.all
+
+    erb :index
+  end
+
+
 
   get '/articles/:id'  do
     @article = Article.find_by(params[:id])
